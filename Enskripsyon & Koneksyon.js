e61a1ca7935e131601228574dd5da5e2bd1a374f
monfistonrobendjy <monfistonrobendjy@gmail.com>
2025-08-14 23:00:29 -0400

    Update Enskripsyon & Koneksyon.js

--- Enskripsyon & Koneksyon.js
+++ Enskripsyon & Koneksyon.js
@@ -5,17 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
     form.addEventListener('submit', function(e) {
         e.preventDefault();
         
-        // Jwenn tout jaden yo
         const inputs = form.querySelectorAll('input');
         const [non, prenon, email, password, confirm] = Array.from(inputs).map(input => input.value);
         
-        // Tcheke si modpas yo matche
         if (password !== confirm) {
             alert('Modpas yo pa matche! Tanpri verifye.');
             return;
         }
         
-        // Kreye objè itilizatè
         const itilizate = {
             non,
             prenon,
@@ -23,10 +20,8 @@ document.addEventListener('DOMContentLoaded', function() {
             password
         };
         
-        // Sove nan localStorage
         localStorage.setItem(email, JSON.stringify(itilizate));
         
-        // Afiche mesaj siksè epi redirije
         alert(`Byenvini ${non} pami nou`);
         window.location.href = 'connexion.html';
     });
@@ -41,12 +36,10 @@ document.addEventListener('DOMContentLoaded', function() {
     
     form.addEventListener('submit', function(e) {
         e.preventDefault();
-        
-        // Valè yo
+       
         const email = form.querySelector('input[type="email"]').value;
         const password = form.querySelector('input[type="password"]').value;
         
-        // Chèche itilizatè nan localStorage
         const itilizateStorage = localStorage.getItem(email);
         
         if (!itilizateStorage) {
@@ -54,19 +47,15 @@ document.addEventListener('DOMContentLoaded', function() {
             return;
         }
         
-        // JSON 
         const itilizate = JSON.parse(itilizateStorage);
         
-        // Verifye modpas
         if (itilizate.password !== password) {
             alert('Modpas ou an pa kòrèk');
             return;
         }
         
-        // Sove sesyon kouran
         sessionStorage.setItem('kounye_a', JSON.stringify(itilizate));
         
-        // Redirije sou paj welkòm
         window.location.href = 'ajouterEtudiant.html';
     });
 });
